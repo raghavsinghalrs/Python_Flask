b877e183cf4784f459a01f819281a6c6c72be010
raghav <raghav.s@inncircles.com>
2023-10-23 12:53:58 +0530

    Calculator app is ready to use

--- pythonapp.py
+++ pythonapp.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
-def saynotociggarates():
-    return "<a href='https://www.google.com/maps'>Ciggarates is injurious to health</a>"
+@app.route("/",methods=['GET','POST'])
+def home_page():
+    return render_template("index.html")
+@app.route('/math',methods=['POST'])
+def math_operation():
+    if(request.method=='POST'):
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if(ops == 'add'):
+            result = num1 + num2
+            output = 'Addition of ' + str(num1) + ' and ' + str(num2) + " is " + str(result)
+        elif(ops == 'subtract'):
+            result = num1-num2
+            output = 'Subtraction of ' + str(num1) + ' and ' + str(num2) + " is " + str(result)
+        elif(ops == 'multiply'):
+            result = num1*num2
+            output = 'Multiplication of ' + str(num1) + ' and ' + str(num2) + " is " + str(result)
+        elif(ops == 'divide'):
+            result = num1/num2
+            output = 'Division of ' + str(num1) + ' and ' + str(num2) + " is " + str(result)
+        return render_template('results.html',result=output)
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
